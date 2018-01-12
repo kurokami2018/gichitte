ddfe2c671969885b6f1f717f33308d93d3fecd83
aasako <34863351+aasako@users.noreply.github.com>
2018-01-12 17:01:50 +0900

    Update monthCheck.java

--- feature/src/main/java/com/example/kurokami/guchitte/feature/monthCheck.java
+++ feature/src/main/java/com/example/kurokami/guchitte/feature/monthCheck.java
@@ -11,18 +11,22 @@ import java.util.Calendar;//日付の操作・計算などを扱うクラス
 import java.util.List;
 import com.google.gson.reflect.TypeToken;
 
-//月(年)が変わった時のデータ取得方法
-//→ 前回ログインしたときの月(month)が保存できれば...！！
+/*
+月(年)が変わった時のデータ取得方法 → 前回ログインしたときの月(month)が保存できれば...！！
+[ログインするたびにmonth(月)を保存する。
+次にログインした時に保存してあったmonthとその日に保存したmonthが違っていたら愚痴カウンターを0に(12月→1月のみ例外、白紙にする)。
+同じだったら呟く画面を表示。]
+※白紙にする=背景画像を挿入する
+*/
 
 /*
 List beforeDate=new ArrayList();
-beforeDate.add(month);
+beforeDate.add(month); わからん
 */
 
 public class monthCheck extends MainActivity {
 
-  public static void getCalender(Calendar cal) {
-    //System.out.println(calender.getTime().toString());
+  public static int getCalender(Calendar cal) {
 
     int year = cal.get(Calendar.YEAR); //年を取得
     int month = cal.get(Calendar.MONTH)+1; //月を取得
@@ -31,5 +35,21 @@ public class monthCheck extends MainActivity {
 
 
   }
-}
+  
+  
 
+  
+  public static void main(String[] args){
+    
+    int pastmonth; //仮に最終ログイン月をpastmonthとする
+    
+    if(pastmonth!=month){
+      if(month==1){/*月間初期画面を表示する*/
+                    /*愚痴回数カウンターを0にするメソッド*/
+      }else{/*愚痴回数カウンタを0にするメソッド*/}};
+    
+    if(pastmonth==month){/*呟く画面を表示*/};
+    
+  }
+
+}
